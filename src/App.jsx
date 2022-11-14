247d4324825e71c892d0f5bf7c3522a25333ea40
jose.ocana <jose.ocana@i3television.es>
2022-11-14 17:20:31 +0100

    Añadida libreria de componentes MUI y realizada configuración para usar junto con tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Button } from '@mui/material'
 import './App.css'
+
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-4xl uppercase text-indigo-500 mb-4">Portfolio Jose María Ocaña</h1>
+      <Button variant="contained" className="bg-black">Contained</Button>
     </div>
   )
 }
