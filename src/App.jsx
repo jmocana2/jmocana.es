79c30c48ad26628857186044fb377ccebb540b3a
jose.ocana <jose.ocana@i3television.es>
2022-12-29 17:15:30 +0100

    Primeras pruebas con Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import { Button } from '@mui/material'
 import './App.css'
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App my-0 mx-auto p-8 max-w-screen-xl text-center">
       <h1 className="text-4xl uppercase text-indigo-500 mb-4">Portfolio Jose María Ocaña</h1>
       <Button variant="contained" className="bg-black">Contained</Button>
     </div>
