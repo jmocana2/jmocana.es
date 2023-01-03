b5fa7cf441fcdfe354e2d1fad3552e09d52a990f
jose.ocana <jose.ocana@i3television.es>
2023-01-03 16:21:15 +0100

    Instalado EsLint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import React from 'react'
 import { Button } from '@mui/material'
 import './App.css'
 
-
-function App() {
+function App () {
   return (
     <div className="App my-0 mx-auto p-8 max-w-screen-xl text-center">
       <h1 className="text-4xl uppercase text-indigo-500 mb-4">Portfolio Jose María Ocaña</h1>
